4eb82b64e4a1b47a395a8e10c24cb257313ddfa3
salfianto28 <120007210+salfianto28@users.noreply.github.com>
2022-12-07 14:57:10 +0700

    menambahkan latihan1

---
+++ latihan1.py
@@ -0,0 +1,18 @@
+a=10
+b=30
+perkalian=a*b
+print(perkalian)
+print(.................)
+
+#dictionari
+nama_mhs = {
+    "nama"   :"yudi",
+    "alamat" :"Batang",
+    "nohp"   : 9880,
+
+}
+print ('___________________________')
+print ('| nama mahasiswa | alamat |')
+print ('___________________________')
+print (nama_mhs ["nama"],nama_mhs['alamat'], nama_mhs ['nohp'])
+print ('___________________________')
